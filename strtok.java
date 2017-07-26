de7286c5f2c2b9d76828a8e044d0993910ef6233
dylnye <30329296+dylnye@users.noreply.github.com>
2017-07-26 01:04:19 -0400

    Add files via upload

---
+++ strtok.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+import java.nio.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+public class strtok {
+
+public static void main(String [] args) {
+	
+	
+	String content = null;
+
+	try {
+		content = new String(Files.readAllBytes(Paths.get("C:\\Users\\Dylan\\Desktop\\Eclipse Files and Programs\\sampleproject\\src\\setup.txt")));
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+
+	String delims = "===";
+    StringTokenizer Tokr = new StringTokenizer(content, delims);
+    int n=0;
+
+    while (Tokr.hasMoreElements())
+         System.out.println("" + ++n +": "+Tokr.nextElement());
+	
+}
+}
